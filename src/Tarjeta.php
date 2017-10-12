c5cfe625ee6cd485dec7a50622bb88922ab3a483
Ignacio Rassol <ignaciorassol@gmail.com>
2017-10-12 00:10:46 -0300

    Commit inicial, declaracion de las clases

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -2,9 +2,36 @@
 
 namespace TpFinal;
 
-class Tarjeta {
-
-    public function saldo() {
-        return 0;
-    }
+interface Tarjeta {
+ 	public function pagar(Transporte $transporte, $fecha_y_hora);
+ 	public function recargar($monto);
+ 	public function saldo();
+ 	public function viajesRealizados();
 }
+
+class Tarjeta implements Tarjeta {
+  
+ 	public function pagar(Transporte $transporte, $fecha_y_hora) {
+ 
+ 	}
+ 
+ 	public function recargar($monto) {
+ 
+ 	}
+ 
+    public function saldo() {
+        	return 0;
+      }
+
+    public function viajes_realizados() {
+ 
+     }
+ }
+ 
+ class Colectivo {
+ 
+ }
+ 
+ class Bicicleta {
+ 
+ } 
