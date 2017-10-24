8230e92441a2ad3d7daf40b0f600b6114477cc09
Warangaron <32685195+Warangaron@users.noreply.github.com>
2017-10-24 17:19:19 -0300

    Hecha la funcion recargar de tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -17,7 +17,17 @@ class Tarjeta implements Tarjeta {
  	}
  
  	public function recargar($monto) {
- 
+  $montosposibles = [10,20,30,50,100,332,624];
+  
+   if (in_array($monto, array_slice($montosposibles,0,5)){
+    $this->carga += $monto;
+   } else if (in_array($monto, $montosposibles)){
+    if ($monto == 332)
+     $this->carga += 388;
+    else
+     $this->carga += 776;
+   }else
+       return "No se puede recargar ese monto";
  	}
  
     public function saldo() {
