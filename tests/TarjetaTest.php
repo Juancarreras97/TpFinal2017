f1bc0287bd999145946172ae0cd8a774c6fe50c1
Warangaron <32685195+Warangaron@users.noreply.github.com>
2017-10-24 17:30:31 -0300

    Faltaban los parentesis al crear la nueva instancia de tarjeta en testsaldocero

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -10,7 +10,7 @@ class EstacionTest extends TestCase {
      * Comprueba que el saldo de una tarjeta nueva sea cero.
      */
     public function testSaldoCero() {
-        $tarjeta = new Tarjeta;
+        $tarjeta = new Tarjeta();
         $this->assertEquals($tarjeta->saldo(), 0);
     }
 }
