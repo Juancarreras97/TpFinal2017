413f04c7454ee6001b9442e731809f88ce6ad288
Ignacio Rassol <ignaciorassol@gmail.com>
2017-10-25 17:52:06 -0300

    Cambiada la funcion viajes realizados para ver si anda travis

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -10,33 +10,34 @@ interface Tarjeta_interfaz {
 }
 
 class Tarjeta implements Tarjeta_interfaz {
-  protected $carga;
+	
+	protected $carga;
   
  	public function pagar(Transporte $transporte, $fecha_y_hora) {
  
  	}
  
  	public function recargar($monto) {
-  $montosposibles = [10,20,30,50,100,332,624];
+		$montosposibles = [10,20,30,50,100,332,624];
   
-   if (in_array($monto, array_slice($montosposibles,0,5))){
-    $this->carga += $monto;
-   } else if (in_array($monto, $montosposibles)){
-    if ($monto == 332)
-     $this->carga += 388;
-    else
-     $this->carga += 776;
-   }else
-       return "No se puede recargar ese monto";
+		if (in_array($monto, array_slice($montosposibles,0,5))){
+			$this->carga += $monto;
+		} else if (in_array($monto, $montosposibles)){
+			if ($monto == 332)
+				$this->carga += 388;
+			else
+				$this->carga += 776;
+		} else
+			return "No se puede recargar ese monto";
  	}
  
     public function saldo() {
-        	return $this->carga;
-      }
+		return $this->carga;
+	}
 
     public function viajes_realizados() {
-     ;
-    }
+    	return 0;
+	}
  }
  
  
